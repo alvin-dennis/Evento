17230b65d218643de53873a6431285961fe666bc
Alvin Dennis <alvindennis80@gmail.com>
2025-07-24 00:18:34 +0530

    feat: removed server setup and started with monorepo setup with react

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@client": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "../server/src"),
-      "@shared": path.resolve(__dirname, "../shared/src"),
       "@": path.resolve(__dirname, "./src")
     }
   }
